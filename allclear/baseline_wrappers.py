bd268f4994aa88990bc78d01709085fc0446ef82
chkao <0102030405jacky@gmail.com>
2024-04-28 00:43:42 -0400

    remove models

--- allclear/baseline_wrappers.py
+++ allclear/baseline_wrappers.py
@@ -35,7 +35,7 @@ class BaseModel(ABC):
 class UnCRtainTS(BaseModel):
     def __init__(self, args):
         super().__init__(args)
-        from model.src.model_utils import get_model, load_checkpoint
+        from UnCRtainTS.model.src.model_utils import get_model, load_checkpoint
         # to_date = lambda string: datetime.strptime(string, "%Y-%m-%d")
         to_date = lambda string: datetime.strptime(string, "%Y-%m-%d").timestamp()
         self.S1_LAUNCH = to_date("2014-04-03")
@@ -53,8 +53,8 @@ class UnCRtainTS(BaseModel):
         pass
 
     def get_config(self):
-        from baselines.UnCRtainTS.model.src.utils import str2list
-        from baselines.UnCRtainTS.model.parse_args import create_parser
+        from UnCRtainTS.model.src.utils import str2list
+        from UnCRtainTS.model.parse_args import create_parser
         parser = create_parser(mode="test")
         conf_path = os.path.join(self.args.baseline_base_path, self.args.weight_folder, self.args.experiment_name, "conf.json")
         with open(conf_path, "r") as f:

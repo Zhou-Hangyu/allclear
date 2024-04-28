dd8b10fce9561b9e3f74a13f8c1dd1287ac905a2
Hangyu Zhou <zhybrid1021@gmail.com>
2024-04-28 10:05:55 -0400

    ignore intermediate folders

--- allclear/.ipynb_checkpoints/__init__-checkpoint.py
+++
@@ -1,4 +0,0 @@
-from .dataset import CRDataset
-from .baseline_wrappers import UnCRtainTS, LeastCloudy, Mosaicing
-from .benchmark import BenchmarkEngine, Metrics
-from .utils import visualize_with_grid, cloud_mask_threshold

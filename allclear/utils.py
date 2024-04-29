fix bug in visualize_with_grid, also improve normalization setting

allclear/utils.py
@@ -31,6 +31,7 @@ def visualize_with_grid(
     lulc_color=None,
     default_opacity=0.5,
     save_dir=None,
+    dpi=100,
 ):
     """
     Visualize a multi-channel MSI with optional overlays and a grid.
@@ -40,6 +41,7 @@ def visualize_with_grid(
             # Load MSI data
             with rs.open(msi) as src:
                 msi_data = src.read()
+                msi_data = np.clip(msi_data/3000, 0, 1)
         else:
             msi_data = msi
     elif sar is not None:
@@ -79,19 +81,19 @@ def visualize_with_grid(
         overlays.append((lulc_data[lulc_channel, ...], lulc_cmap))
 
     # Setup figure
-    plt.figure(figsize=(12, 12), dpi=200)
+    plt.figure(figsize=(12, 12), dpi=dpi)
 
     if msi is not None:
         # Process and display MSI image
         # p2, p98 = np.percentile(msi_data[msi_channels, ...], [2, 98])
         # msi_normalized = np.clip((msi_data[msi_channels, ...] - p2) / (p98 - p2), 0, 1)
         # plt.imshow(msi_normalized.permute(1, 2, 0), interpolation="nearest")
-        plt.imshow(msi[msi_channels, ...].permute(1, 2, 0), interpolation="nearest", vmin=0, vmax=1)
+        plt.imshow(msi_data[msi_channels, ...].transpose((1, 2, 0)), interpolation="nearest", vmin=0, vmax=1)
     elif sar is not None:
         # Process and display SAR image
         p2, p98 = np.percentile(sar_data[sar_channels, ...], [2, 98])
         sar_normalized = np.clip((sar_data[sar_channels, ...] - p2) / (p98 - p2), 0, 1)
-        plt.imshow(sar_normalized.permute(1, 2, 0), interpolation="nearest", cmap="gray")
+        plt.imshow(sar_normalized.transpose((1, 2, 0)), interpolation="nearest", cmap="gray")
 
     # Apply overlays
     for overlay_data, color in overlays:
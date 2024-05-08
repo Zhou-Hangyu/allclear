9c2087c6dd5277697b05bcc6a4e57ffc54f4e1e0
chkao <0102030405jacky@gmail.com>
2024-05-07 23:34:28 -0400

    checked ctgan

--- allclear/baseline_wrappers.py
+++ allclear/baseline_wrappers.py
@@ -371,7 +371,7 @@ class CTGAN(BaseModel):
         super().__init__(args)
 
         args.image_size = 256
-        args.load_gen = '/share/hariharan/ck696/allclear/baselines/CTGAN/CTGAN/CTGAN/Sen2_MTC/Pretrain/CTGAN-Sen2_MTC/G_epoch97_PSNR21.259-002.pth'
+        args.load_gen = '/share/hariharan/ck696/allclear/baselines/CTGAN/Pretrain/G_epoch97_PSNR21.259-002.pth'
         
         self.image_size = args.image_size
         self.load_gen = args.load_gen
@@ -386,7 +386,8 @@ class CTGAN(BaseModel):
         self.model.eval()
 
         # Bands: R, G, B, NIR
-        self.bands = (3,2,1,7)
+        # self.bands = (3,2,1,7)
+        self.bands = (1,2,3,7)
 
     def get_model_config(self):
         pass
@@ -406,7 +407,7 @@ class CTGAN(BaseModel):
         """
         # Size of the model input is (T, BS, C, H, W)
         # Size of the model output is (BS, C, H, W)
-        input_imgs = inputs["input_images"].permute(1, 0, 2, 3, 4)[:,:,self.bands] * 2 - 1
+        input_imgs = inputs["input_images"][:,:,self.bands] * 2 - 1
         self.model = self.model.to(self.device)
         output, _, _ = self.model(input_imgs)
         output = output.unsqueeze(1) * 0.5 + 0.5
@@ -433,6 +434,38 @@ class UTILISE(BaseModel):
             checkpoint = '/share/hariharan/cloud_removal/allclear/baselines/UTILISE/checkpoints/utilise_sen12mscrts_wo_s1.pth'
 
         
+        from baselines.UTILISE.lib.eval_tools import Imputation
+        utilise = Imputation(config_file_train, method='utilise', checkpoint=checkpoint)
+        self.model = utilise.model.to(self.device)
+        self.model.eval()
+        print("Note!!! Using UTILISE is a seq-to-seq model. Using the middle frame fro prediction may not be accurate.")
+
+    def get_model_config(self):
+        pass
+
+    def preprocess(self, inputs):
+        inputs["input_images"] = torch.clip(inputs["input_images"]/10000, 0, 1).to(self.device)
+        inputs["target"] = torch.clip(inputs["target"]/10000, 0, 1).to(self.device)
+        return inputs
+
+    def forward(self, inputs):
+        """Refer to `prepare_data_multi()`
+        Shapes:
+            - input_imgs: (B, T, C, H, W)
+            - target_imgs: (B, 1, C, H, W)
+            - masks: (B, T, H, W)
+            - dates: (B, T)
+        """
+        # Model I/O (bs, t, c, h, w)
+        output = self.model(inputs["input_images"])[:,2:3]
+        return {"output": output}
+    
+
+
+class PMAA(BaseModel):
+    def __init__(self, args):
+        super().__init__(args)
+
         from baselines.UTILISE.lib.eval_tools import Imputation
         utilise = Imputation(config_file_train, method='utilise', checkpoint=checkpoint)
         self.model = utilise.model.to(self.device)
--- demos/run_benchmark_pmaa.sh
+++ demos/run_benchmark_pmaa.sh
@@ -7,16 +7,18 @@ SCRIPT_PATH="allclear/benchmark.py"
 BASELINE_BASE_PATH='/share/hariharan/cloud_removal/allclear/baselines/UnCRtainTS'
 DATA_PATH="/share/hariharan/cloud_removal/metadata/split_temp/test_s2_2022_1-2022_12_patches_t3.csv"
 METADATA_PATH="/share/hariharan/cloud_removal/metadata/patch_temp/test_s2_2022_1-2022_12_ts_cr_more_metadata.csv"
-MODEL_NAME="ctgan"
+MODEL_NAME="pmaa"
 BATCH_SIZE=4
 NUM_WORKERS=2
 DEVICE="cuda:0"
+# EVAL_MODE="sr"
 EVAL_MODE="toa"
 
-SELECTED_ROIS='0 1 14 21 29 31 34 35 37 38 50 51 52' # RGB+NIR {'MAE': 0.06823699921369553, 'RMSE': 0.08542048186063766, 'PSNR': 22.330751419067383, 'SAM': 16.234838485717773, 'SSIM': 0.6532679796218872}
-# SELECTED_ROIS='0 1'                                    # RGB+NIR {'MAE': 0.10186664760112762, 'RMSE': 0.11121731251478195, 'PSNR': 19.924110412597656, 'SAM': 10.919565200805664, 'SSIM': 0.645341694355011}
-# SELECTED_ROIS='0 1'                                    # BGR+NIR {'MAE': 0.11048389226198196, 'RMSE': 0.12355108559131622, 'PSNR': 19.606834411621094, 'SAM': 10.542755126953125, 'SSIM': 0.615655243396759}
-EXP_OUTPUT_PATH="/share/hariharan/cloud_removal/results/baselines/CTGAN/outputs"
+SELECTED_ROIS='0 1 14 21 29 31 34 35 37 38 50 51 52' # 13 test rois from sen12ms-cr-ts
+# SELECTED_ROIS='14' # 13 test rois from sen12ms-cr-ts
+# SELECTED_ROIS='1' # 13 test rois from sen12ms-cr-ts
+EXP_OUTPUT_PATH="/share/hariharan/cloud_removal/results/baselines/PMAA/outputs"
+PMAA_MODEL="new"
 
 
 python $SCRIPT_PATH \
@@ -30,4 +32,5 @@ python $SCRIPT_PATH \
   --selected-rois $SELECTED_ROIS \
   --experiment-output-path $EXP_OUTPUT_PATH \
   --save-plots \
-  --eval-mode $EVAL_MODE
+  --eval-mode $EVAL_MODE \
+  --pmaa-model $PMAA_MODEL

31565200819156da0770f3f1b406c91a3b975f6b
Hangyu Zhou <zhybrid1021@gmail.com>
2024-05-16 20:43:39 -0400

    fix bug to recover error files

--- dataset/download_data_fix_error.sh
+++ dataset/download_data_fix_error.sh
@@ -6,26 +6,27 @@ EE_PROJECT_ID="ee-zhybrid1021"
 DATA_TYPES=("s2_toa" "s1" "dw" "cld_shdw" "landsat8")
 ROOT="/share/hariharan/cloud_removal/MultiSensor/dataset_30k_v4"
 #ROIS_PATH="/share/hariharan/cloud_removal/metadata/tile/roi_central_coordinates_sen12mscrts_sen12mscr.csv"
-ROIS_PATH="/share/hariharan/cloud_removal/metadata/tile/v2_distribution_train_30Ksamples.csv"
+ROIS_PATH="/share/hariharan/cloud_removal/metadata/train_2k.csv"
 #ROIS_PATH="/share/hariharan/cloud_removal/metadata/tile/v2_distribution_test_6Ksamples-2.csv"
 START_DATE="2022-01-01"
 END_DATE="2022-12-31"
 START_ROI=0
-#WORKERS=16
+WORKERS=16
 #WORKERS=32
-WORKERS=1
+#WORKERS=1
 #WORKERS=62
 
 
-python dataset/download_data.py \
-    --ee-project-id $EE_PROJECT_ID \
-    --data-type $DATA_TYPE \
-    --root $ROOT \
-    --rois $ROIS_PATH \
-    --start-date $START_DATE \
-    --end-date $END_DATE \
-    --start-roi $START_ROI \
-    --end-roi $END_ROI \
-    --workers $WORKERS \
-    --quiet \
-    --resume
+for DATA_TYPE in "${DATA_TYPES[@]}"; do
+    python dataset/download_data.py \
+        --ee-project-id $EE_PROJECT_ID \
+        --data-type $DATA_TYPE \
+        --root $ROOT \
+        --rois $ROIS_PATH \
+        --start-date $START_DATE \
+        --end-date $END_DATE \
+        --start-roi $START_ROI \
+        --workers $WORKERS \
+        --quiet \
+        --resume
+done
